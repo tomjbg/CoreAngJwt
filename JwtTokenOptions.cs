f80acae2d9c0daeaed8cd7f8ba9c09af5756f6bf
TomJGB <tgds.net@outlook.com.br>
2018-01-14 22:28:10 -0200

    Authentication_Authorization_TokenBearer

--- JwtTokenOptions.cs
+++ JwtTokenOptions.cs
@@ -1,11 +1,19 @@
+using System;
+using System.Threading.Tasks;
 using Microsoft.IdentityModel.Tokens;
 
 namespace CoreAngJwt
 {
     public class JwtTokenOptions
     {
-        public string Issuer { get; set; }
-        public string Audience { get; set; }    
+        public string Issuer { get; set; } // Emissor do token
+        public string Subject { get; set; }  // Assunto
+        public string Audience { get; set; }  // Para qual site este token é valido, https://site.domain.com por exemplo
+        public DateTime NotBeforce { get; set; } = DateTime.UtcNow; // Não utilizável antes desta data
+        public DateTime IssuedAt { get; set; } = DateTime.UtcNow; // Quando ele foi emitido
+        public TimeSpan ValidFor { get; set; } = TimeSpan.FromHours(5); // Por quando tempo ele é válido. No caso 5 horas de validade para o Token
+        public DateTime Expiration => IssuedAt.Add(ValidFor); // Expiração dele que é de quando ele foi emitido até o tempo de duração do token.
+        public Func<Task<string>> JtiGenerator => () => Task.FromResult(Guid.NewGuid().ToString()); // Task Generator que gera a string de um novo Guid
         public SigningCredentials signingCredentials { get; set; }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -17,6 +17,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using System.IO;
 
 namespace CoreAngJwt
 {
@@ -28,23 +29,31 @@ namespace CoreAngJwt
         }
 
         public IConfiguration Configuration { get; }
-        private const string SecretKey = "Secret@SenhaToken";
+        private const string SecretKey = "SecretToken@123";
         private readonly SymmetricSecurityKey _signingkey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(SecretKey));
 
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //  BANCO DBCONTEXT
+            // Adicionando o DBContext e Definindo o tipo de Banco de dados SQL Lite
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlite(Configuration.GetConnectionString("core_ang_jwt_DB")));
-
+            
+            //  IDENTITY
+            // Adicionando o Identity e definindo que vai usar provedor de token padrão
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
-            // Add application services.
+            //  SERVIÇOS TRANSITÓRIOS
+            // Add application services. Adicionando um serviço do tipo Transitório
             services.AddTransient<IEmailSender, EmailSender>();
 
+            //  FILTERS OPTIONS
+            // Adicionando o Serviço, a funcionalidade MVC e definindo que o retorno não será do tipo Xml e 
+            //que nas requisições é exigido um Token JwtBearer e usuário autenticado.
             services.AddMvc(opt => {
                 opt.OutputFormatters.Remove(new XmlDataContractSerializerOutputFormatter());
                 
@@ -57,14 +66,18 @@ namespace CoreAngJwt
 
             });
 
-            
-
+            //  AUTHORIZATION
+            // Definindo regras de Autorização
             services.AddAuthorization(opt => {
                     opt.AddPolicy("PodeLer", policy => policy.RequireClaim("Autonomia", "Ler"));
                     opt.AddPolicy("PodeModificar", policy => policy.RequireClaim("Autonomia", "Modificar"));
                     opt.AddPolicy("PodeTudo", policy => policy.RequireClaim("Autonomia", "Total"));
             });
 
+            //  AUTHENTICATION
+
+            //  TOKEN
+            // JWT BEARER TOKEN
             var jwtAppSettingOptions = Configuration.GetSection(nameof(JwtTokenOptions));
 
             services.Configure<JwtTokenOptions>(opt => {
@@ -72,6 +85,32 @@ namespace CoreAngJwt
                 opt.Audience = jwtAppSettingOptions[nameof(JwtTokenOptions.Audience)];
                 opt.signingCredentials = new SigningCredentials(_signingkey, SecurityAlgorithms.HmacSha256);
             });
+
+            // Parametros do Token, definições
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true, // Sim, valide o Emissor
+                ValidIssuer = jwtAppSettingOptions[nameof(JwtTokenOptions.Issuer)], // Atribuindo quem é o Emissor deste Toker
+
+                ValidateAudience = true, // Sim, valide o endereço o site Emissor deste Token
+                ValidAudience = jwtAppSettingOptions[nameof(JwtTokenOptions.Audience)], // Atribuindo quem é o endereço o site Emissor deste Token
+
+                ValidateIssuerSigningKey = true, // Sim, valide a Assinatura do Emissor
+                IssuerSigningKey = _signingkey, // Atribuindo a Assinatura para ser validado
+
+                RequireExpirationTime = true, // Sim, requer um Tempo de Expiração
+                ValidateLifetime = true, // Sim, Valide Tempo de Vida
+                
+                ClockSkew = TimeSpan.Zero // Estabelecendo o Marco Zero do Relógio, no caso inicia no Zero
+            };
+
+            // Adicionando o Serviço Autenticação, definindo o tipo de autenticação como JwtBearer Token e 
+            // aplicando os parametros de validação do token
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(opt => { 
+                        opt.TokenValidationParameters = tokenValidationParameters;
+                    });
+
 
         }
 
@@ -88,30 +127,30 @@ namespace CoreAngJwt
                 app.UseExceptionHandler("/Home/Error");
             }
 
-            var jwtAppSettingOptions = Configuration.GetSection(nameof(JwtTokenOptions));
+            app.Use(async (context , next) => {
+               await next();
+               if(context.Response.StatusCode == 404 &&
+                  !Path.HasExtension(context.Request.Path.Value) && 
+                  !context.Request.Path.Value.StartsWith("/api/")) {
+                      context.Request.Path = "/index.html";
+                      await next();
+               }
+            });
 
-            var tokenValidationParameters = new TokenValidationParameters
-            {
-                ValidateIssuer = true,
-                ValidIssuer = jwtAppSettingOptions[nameof(JwtTokenOptions.Issuer)],
-
-                ValidateAudience = true,
-                ValidAudience = jwtAppSettingOptions[nameof(JwtTokenOptions.Audience)],
-
-                ValidateIssuerSigningKey = true,
-                IssuerSigningKey = _signingkey,
-
-                RequireExpirationTime = true,
-                ValidateLifetime = true,
-
-                ClockSkew = TimeSpan.Zero
-            };
+            // CORS -> Cross-origin resource sharing = Compartilhamento de Recursos de Origem Cruzada
+            // Determinamos por exemplo quais domínios (nomesite.dominio.com) podem fazer Request para nosso site
+            app.UseCors(c => { 
+                c.AllowAnyHeader(); // Permitir qualquer Header
+                c.AllowAnyMethod(); // Permitir qualquer Método
+                c.AllowAnyOrigin(); // Permitir qualquer Origem
+            });
 
             app.UseStaticFiles();
             app.UseDefaultFiles();
             app.UseMvcWithDefaultRoute();
 
             app.UseAuthentication();
+
 
             // app.UseMvc(routes =>
             // {
